0928ad4375666cd98886106511234ff188edc91c
Manny <manuelpo.msn.com>
2024-01-09 13:30:18 -0700

    all done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name === name;
+  });
+}
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase().startsWith(letters.toLowerCase());
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase().startsWith(letters.toLowerCase());
+  });
+}
+
+module.exports = { findMatching, fuzzyMatch, matchName };
